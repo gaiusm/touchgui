0d9152bddd37e7ace76b926f1eee916da9411d7e
gaius <gaius@gnu.org>
2018-11-22 00:05:53 +0000

    added boilerplate licences.

--- touchgui.py
+++ touchgui.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python
+#
+# touchgui.py a simple touch gui for Pygame.
+#
+# Copyright (C) 2018 Free Software Foundation, Inc.
+# Contributed by Gaius Mulley <gaius@glam.ac.uk>.
+#
+# This file is part of TouchGui.
+#
+# TouchGui is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+#
+# TouchGui is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with GNU Modula-2; see the file COPYING.  If not, write to the
+# Free Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+#
 
 debugging = False
 debugging = True
